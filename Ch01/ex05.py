69ac0e00d5040782b5db9ea46cd74492197c9519
vpapg <vpapgl@gmail.com>
2018-09-17 16:50:07 +0100

    Error fixed

--- Ch01/ex05.py
+++ Ch01/ex05.py
@@ -9,13 +9,13 @@ Created on Mon Jun 11 23:15:17 2018
 # Compare the lexical diversity scores for humor and romance 
 # fiction in 1.1. Which genre is more lexically diverse?
 
-from nltk.book import *
+from nltk import corpus
 
 def lexical_diversity(text):
     return len(set(text)) / len(text)
 
-humor_words = nltk.corpus.brown.words(categories="humor")
-romance_words = nltk.corpus.brown.words(categories="romance")
+humor_words = corpus.brown.words(categories="humor")
+romance_words = corpus.brown.words(categories="romance")
 
 print("Humor:",lexical_diversity(humor_words))
 print("Romance:",lexical_diversity(romance_words))
